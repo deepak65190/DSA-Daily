72202016347c148700ecc1d8bb0ff45f4e1fc1ea
deepak65190 <deepakbisht9773724383@gmail.com>
2026-01-27 23:27:04 +0530

    sum

--- day26Jan.js
+++ day26Jan.js
@@ -1,3 +1,8 @@
 // Reverse an array
-let array=[1,2,3] 
-array.reverse()
+// let array=[1,2,3] 
+// array.reverse()
+//Sum of all elements
+
+let arr=[1,2,3]
+let ans=arr.reduce((a,el)=>a+el)
+console.log(ans)
