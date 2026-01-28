82133ab7c0f5b3ed710efde25e3b14d2d8c4aef5
deepak65190 <deepakbisht9773724383@gmail.com>
2026-01-28 22:48:08 +0530

    length

--- day26Jan.js
+++ day26Jan.js
@@ -3,6 +3,13 @@
 // array.reverse()
 //Sum of all elements
 
+// let arr=[1,2,3]
+// let ans=arr.reduce((a,el)=>a+el)
+// console.log(ans)
+//Sum of all elements
+let length=0 ;
 let arr=[1,2,3]
-let ans=arr.reduce((a,el)=>a+el)
-console.log(ans)
+for(let i=0 ; i<arr.length ; i++){
+    length++
+}
+console.log(length)
