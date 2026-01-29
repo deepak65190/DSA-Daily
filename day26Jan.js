695f5edab8b01372cb9ea7856d1d0aa7ef6b0f3b
deepak65190 <deepakbisht9773724383@gmail.com>
2026-01-29 23:35:26 +0530

    copy array

--- day26Jan.js
+++ day26Jan.js
@@ -7,9 +7,13 @@
 // let ans=arr.reduce((a,el)=>a+el)
 // console.log(ans)
 //Sum of all elements
-let length=0 ;
-let arr=[1,2,3]
-for(let i=0 ; i<arr.length ; i++){
-    length++
-}
-console.log(length)
+//let length=0 ;
+// let arr=[1,2,3]
+// for(let i=0 ; i<arr.length ; i++){
+//     length++
+// }
+// console.log(length)
+// Copy array elements
+// let array=[1,2,3]
+// let b=[...array]
+// console.log(b)
