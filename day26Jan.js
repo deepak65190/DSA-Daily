adf257325669b396cdad0137e646041e6eef7226
deepak65190 <bisht65190@gmail.com>
2026-02-15 22:56:17 +0530

    shorted

--- day26Jan.js
+++ day26Jan.js
@@ -78,3 +78,27 @@
 //     e--
 // }
 // console.log(array)
+// Merge two sorted arrays
+
+// let a=[1,3,5]
+// let b=[2,4,6]
+// let c=[]
+// let i=0 ;
+// let j=0
+// while(i<a.length && j<b.length){
+//     if(a[i]<b[j]){
+//         c.push(a[i])
+//         i++
+//     }else{
+//         c.push(b[j])
+//         j++
+//     }}
+// while(i<a.length){
+//     c.push(a[i])
+//     i++
+// }
+// while(j<b.length){
+//     c.push(b[j])
+//     j++
+// }
+// console.log(c)
