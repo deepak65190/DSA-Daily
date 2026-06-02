61f20460360676e634672112472264d860530db2
deepak65190 <deepakbisht9773724383@gmail.com>
2026-06-03 00:51:52 +0530

    flat

---
+++ day06June.js
@@ -0,0 +1,8 @@
+let array=[[1,3,2],[3,4,5],[6,7,8]];
+// console.log(array.flat(1))
+// console.log(array.reduce((a,b)=>a.concat(b)));
+// let result=[]
+// for(let i=0;i<array.length;i++){
+//     result.push(...array[i])
+
+// }
