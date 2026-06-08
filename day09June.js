a6bd60e557a591c47f4efb9c50a7a27179d2bed3
deepak65190 <deepakbisht9773724383@gmail.com>
2026-06-09 00:46:38 +0530

    string

---
+++ day09June.js
@@ -0,0 +1,28 @@
+// Reverse a string
+// Check if a string is palindrome
+// Find length of string without using length
+// Count vowels and consonants
+// Convert lowercase to uppercase (without built-in)
+// Remove spaces from a string
+// Check if two strings are equal
+// Count words in a sentence
+// Find first and last character of a string
+// Replace all spaces with
+
+//1
+
+let str="hello"
+//console.log(str.split("").reverse().join(""))
+// let reverseStr="" ;
+// for(let i =str.length-1 ; i>=0 ; i--){
+//     reverseStr+=str[i]
+// } ; 
+// console.log(reverseStr)
+// let start=0 ;
+// let end=str.length-1 ;
+// while(start<end){
+//     [str[start],str[end]]=[str[end],str[start]]
+//     start++ ;
+//     end-- ;
+// }
+// console.log(str)
