c4c2f70db93d5de0112f2c53b60b5df4ad21b06d
deepak65190 <bisht65190@gmail.com>
2026-06-09 15:28:54 +0530

    string palindrone

--- day09June.js
+++ day09June.js
@@ -7,11 +7,11 @@
 // Check if two strings are equal
 // Count words in a sentence
 // Find first and last character of a string
-// Replace all spaces with
+// Replace all spaces with 1
 
-//1
 
-let str="hello"
+
+//let str="hello"
 //console.log(str.split("").reverse().join(""))
 // let reverseStr="" ;
 // for(let i =str.length-1 ; i>=0 ; i--){
@@ -26,3 +26,42 @@ let str="hello"
 //     end-- ;
 // }
 // console.log(str)
+
+/*let str="madam"
+let left=0 ;
+let right=str.length-1
+while(left<right){
+    if(str[left]!==str[right]){
+        console.log("no")
+        return 
+        
+    }
+    left++ ;
+    right--
+}*/
+
+/*
+let str="Hello123!"
+let vowels="aeiou"
+let constant ="bcdfghjklmnpqrstvwxyz"
+let v=0 ;
+let c=0 ;
+for(let char of str.toLowerCase()){
+    if(/[a-z]/.test(char)){
+        vowels.includes(char)?v++:c++
+    }
+}
+console.log(v ,c)*/
+
+// Convert lowercase to uppercase
+/*let str="HEllo"
+let result=""
+for(let char of str){
+    let code =char.charCodeAt()
+    if(code>=97 && code<=122){
+        result+=String.fromCharCode(code-32)
+    }else{
+        result+=char
+    }
+}
+console.log(result)*/
